a343eb5c0565fa924559dd1892cfde5cf13acb4d
A2lam <mohamedallam31@hotmail.com>
2017-02-26 23:30:14 +0100

    Creation de la classe Administrateur

---
+++ src/Models/Administrateur.java
@@ -0,0 +1,19 @@
+package Models;
+
+public class Administrateur extends Utilisateur
+{
+	//Déclaration
+	
+	private int administrateurId;
+	private String administrateurNom;
+	private String administrateurPrenom;
+	private int adresseNoRue;
+	private String adresseRue;
+	private String adresseComplement;
+	private int adresseCdePostal;
+	private String adresseVille;
+	private String adressePays;
+	private String administrateurMail;
+	private String administrateurTel;
+	private Utilisateur utilisateur;
+}
