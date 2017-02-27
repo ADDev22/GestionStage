2b431ff3bafd6c4fa6ace9025b30e9fbfc66f287
DRAME <arouna.drame@hotmail.com>
2017-02-27 08:36:12 +0100

    Cree exit z

---
+++ src/Models/SingletonConnection.java
@@ -0,0 +1,23 @@
+package Models;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class SingletonConnection {
+	 private  static Connection  connection;
+	 static {
+		 try {
+			 Class.forName("com.mysql.jdbc.Driver");
+			 connection=DriverManager.getConnection("jdbc:mysql://localhost:8889/DB_CATPROD","root1","");
+			// System.out.println("Creation d'une connexion");
+		 } catch (Exception e) {
+			e.printStackTrace();
+		}
+	 }
+	 public static Connection getConnection()
+	 {
+		// System.out.println("Creation d'une connexion");
+		 return connection;
+	 }
+
+}
