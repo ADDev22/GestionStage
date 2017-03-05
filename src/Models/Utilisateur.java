52615339cb5746635e984525273315f33e0514e4
A2lam <mohamedallam31@hotmail.com>
2017-03-05 23:58:32 +0100

    Developpement de la methode connexion utilisateur

--- src/Models/Utilisateur.java
+++ src/Models/Utilisateur.java
@@ -1,45 +1,138 @@
 package Models;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.mysql.jdbc.Connection;
+import com.mysql.jdbc.Statement;
+
 public class Utilisateur
 {
 	//Déclaration
 	
-		protected int utilisateurId;
-		protected String utilisateurUserName;
-		protected String utilisateurMdp;
-		protected Fonction fonction;
+	protected int utilisateurId;
+	protected String utilisateurUserName;
+	protected String utilisateurMdp;
+	protected Fonction fonction;
 		
-		//Getter
+	//Getter
 		
-		public int getUitilisateurId()
+	public int getUitilisateurId()
+	{
+		return this.utilisateurId;
+	}
+		
+	public String getUtilisateurUserName()
+	{
+		return this.utilisateurUserName;
+	}
+		
+	public String getUtilisateurMdp()
+	{
+		return this.utilisateurMdp;
+	}
+		
+	public Fonction getFonction()
+	{
+		return this.fonction;
+	}
+		
+	//Setter
+		
+	public void setUtilisateurId(int utilisateurId)
+	{
+		this.utilisateurId = utilisateurId;
+	}
+		
+	public void setUtilisateurUserName(String utilisateurUserName)
+	{
+		this.utilisateurUserName = utilisateurUserName;
+	}
+		
+	public void setUtilisateurMdp(String utilisateurMdp)
+	{
+		this.utilisateurMdp = utilisateurMdp;
+	}
+		
+	public void setFonction(Fonction fonction)
+	{
+		this.fonction = fonction;
+	}
+		
+	//Authentification
+	@Override
+	public Boolean connexion(String userName, String mdp)
+	{
+		Connection conn = null;
+		List<Utilisateur> listeUtilisateur = new ArrayList<>();
+		String sql = "SELECT * FROM utilisateur";
+		try
 		{
-			return this.utilisateurId;
-		}
+			conn = DBConnection.getConnection();
+			Statement s = (Statement) conn.createStatement();
+			ResultSet rs = s.executeQuery(sql);
+			while (rs.next())
+			{
+				listeUtilisateur.add(traitementLigne(rs));
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+	        throw new RuntimeException(e);
+	    } finally {
+	        DBConnection.close(conn);
+	    }
+			
+		for (Utilisateur u : listeUtilisateur)
+	    {
+	        if (u.getUtilisateurUserName() == userName)
+	            if (u.getUtilisateurMdp() == mdp)
+	            	return true;
+	    }
+			
+		return false;		
+	}
 		
-		public String getUtilisateurUserName()
-		{
-			return this.utilisateurUserName;
-		}
+	//Traitement du resultat renvoyé par une requête --> correspodance avec un objet
+	private Utilisateur traitementLigne(ResultSet rs) throws SQLException 
+	{
+		Utilisateur utilisateur = new Utilisateur();
+		utilisateur.setUtilisateurId(rs.getInt("utilisateurId"));
+		utilisateur.setUtilisateurUserName(rs.getString("utilisateurUserName"));
+		utilisateur.setUtilisateurMdp(rs.getString("utilisateurMdp"));
+		utilisateur.setFonction(getFonction(rs.getInt("fonction_fonctionId")));
+		    
+		return utilisateur;
+	}
 		
-		public Fonction getFonction()
-		{
-			return this.fonction;
-		}
-		
-		//Setter
-		
-		public void setUtilisateurUserName(String utilisateurUserName)
-		{
-			this.utilisateurUserName = utilisateurUserName;
-		}
-		
-		public void setUtilisateurMdp(String utilisateurMdp)
-		{
-			this.utilisateurMdp = utilisateurMdp;
-		}
-		
-		public void setFonction(Fonction fonction)
-		{
-			this.fonction = fonction;
-		}
+	//Recupération des information de la fonction à partir de l'Id
+	private Fonction getFonction(int fonctionId)
+	{
+		Connection conn = null;
+		PreparedStatement ps = null;
+		Fonction fonction = new Fonction();
+		String sql = "SELECT * FROM fonction WHERE fonctionId = ?";
+			
+		try {
+			conn = DBConnection.getConnection();
+			ps = conn.prepareStatement(sql);			
+			ps.setInt(1, fonctionId);			
+			ResultSet rs = ps.executeQuery();
+				
+			if (rs.next())
+			{
+				fonction.setFonctionId(rs.getInt("fonctionId"));
+				fonction.setFonctionNom(rs.getString("fonctionNom"));
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+	        throw new RuntimeException(e);
+	    } finally {
+	        DBConnection.close(conn);
+	    }
+			
+		return fonction;
+	}
 }
