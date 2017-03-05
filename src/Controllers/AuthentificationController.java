4e8fc30b530cdab5613d4448d7c65f1cfcdd1a77
A2lam <mohamedallam31@hotmail.com>
2017-03-05 23:46:53 +0100

    Creation du controleur de l'authentification

---
+++ src/Controllers/AuthentificationController.java
@@ -0,0 +1,30 @@
+package Controllers;
+
+import Models.IAuthentification;
+import Models.Utilisateur;
+
+public class AuthentificationController {
+	IAuthentification iAuth = (IAuthentification) new Utilisateur();
+	private String userName;
+	private String mdp;
+	
+	public AuthentificationController()
+	{
+		//
+	}
+	
+	public void setUserName(String userName)
+	{
+		this.userName = userName;
+	}
+	
+	public void setMdp(String mdp)
+	{
+		this.mdp = mdp;
+	}
+	
+	public void authentification()
+	{
+		if(iAuth.connexion(this.userName, this.mdp)){}
+	}
+}
