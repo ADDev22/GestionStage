1108d795d46e5a9bfe94d5cc1ffc96131462eb9e
A2lam <mohamedallam31@hotmail.com>
2017-03-19 23:53:49 +0100

    Ajout du bouton Inscription

--- src/Views/Authentification.java
+++ src/Views/Authentification.java
@@ -16,6 +16,7 @@ import javax.swing.JPasswordField;
 import javax.swing.JTextField;
 
 import Controllers.AuthentificationController;
+import Models.DAO.UtilisateurDAO;
 
 public class Authentification extends JFrame
 {
@@ -26,25 +27,27 @@ public class Authentification extends JFrame
 	private JLabel labelNomU = new JLabel("Nom d'utilisateur");
 	private JLabel labelMdp = new JLabel("Mot de passe");
 	private JButton c = new JButton ("Connexion");
+	private JButton i = new JButton("Inscription");
 	
-	//Instanciation de notre objet contrôleur
-	private AuthentificationController controller;
+	//Instanciation de notre objet contrÃ´leur
+	private AuthentificationController authController;
 
 	public Authentification(AuthentificationController authController)
 	{
-		this.controller = authController;
-		
+		this.authController = authController;
+
 	    this.setTitle("Gestion Stage");
-	    this.setSize(300, 200);
+	    this.setSize(300, 180);
+	    this.setResizable(false);
 	    this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	    this.setLocationRelativeTo(null);
-	    
+
 	    container.setBackground(Color.white);
 	    container.setLayout(new BorderLayout());
-	    
+
 	    JPanel top = new JPanel();
 	    top.add(label);
-	    
+
 	    JPanel center = new JPanel();
 	    center.setLayout(new BorderLayout());
 	    	JPanel ligneNomU = new JPanel();
@@ -55,36 +58,49 @@ public class Authentification extends JFrame
 	    	ligneMdp.add(mdp);
 	    center.add(ligneNomU, BorderLayout.NORTH);
 	    center.add(ligneMdp, BorderLayout.SOUTH);
-	    
+
 	    JPanel south = new JPanel();
-	    south.add(c);	    	
-	    
-	    Font police = new Font("Arial", Font.BOLD, 14);
-	    label.setFont(police);
-	    labelNomU.setFont(police);
+	    south.add(c);
+	    south.add(i);
+
+	    //Font police = new Font("Arial", Font.BOLD, 14);
+	    //label.setFont(police);
+	    //labelNomU.setFont(police);
 	    userName.setPreferredSize(new Dimension(150, 30));
-	    labelMdp.setFont(police);
+	    //labelMdp.setFont(police);
 	    mdp.setPreferredSize(new Dimension(150, 30));
 	    //jtf.setForeground(Color.BLUE);
-	    
+
 	    c.addActionListener(new BoutonConnexionListener());
-	    
+	    i.addActionListener(new BoutonInscriptionListener());
+
 	    container.add(top, BorderLayout.NORTH);
 	    container.add(center, BorderLayout.CENTER);
 	    container.add(south, BorderLayout.SOUTH);
-	    
+
 	    this.setContentPane(container);
-	    this.setVisible(true);            
+	    this.setVisible(true);
 	}       
 
 	class BoutonConnexionListener implements ActionListener
 	{
 	    public void actionPerformed(ActionEvent e)
 	    {
-	    	controller.setUserName(userName.getText());
-	    	controller.setMdp(mdp.getText());
-	    	
-	    	controller.authentification();
+	    	authController.setUserName(userName.getText());
+	    	authController.setMdp(mdp.getText());
+
+	    	authController.authentification();
 	    }
+	}
+
+	class BoutonInscriptionListener implements ActionListener
+	{
+		public void actionPerformed(ActionEvent e)
+		{
+			UtilisateurDAO uDAO = new UtilisateurDAO();
+			AuthentificationController authController = new AuthentificationController(uDAO);
+			Inscription inscription = new Inscription(authController);
+			Authentification.this.dispose();
+		}
 	}
 }
