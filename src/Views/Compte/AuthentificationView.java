5c427b67a28875089f2dca2b20bf281cdf48cb64
A2lam <mohamedallam31@hotmail.com>
2017-03-24 21:55:01 +0100

    Creation vue Authentification

--- src/Views/Compte/AuthentificationView.java
+++ src/Views/Compte/AuthentificationView.java
@@ -1,7 +1,63 @@
 package Views.Compte;
 
+import Controllers.AuthentificationController;
+import Models.DAO.UtilisateurDAO;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 /**
  * Created by Allam on 24/03/2017.
  */
-public class AuthentificationView {
+public class AuthentificationView extends JFrame
+{
+    private JLabel lAnnonce;
+    private JLabel lAuthentification;
+    private JLabel lUserName;
+    private JLabel lMdp;
+    private JTextField tfUserName;
+    private JTextField tfMdp;
+    private JButton bConnexion;
+    private JButton bInscription;
+    private JPanel container;
+    private JPanel pBouttons;
+    private JPasswordField pfMdp;
+
+    //Instanciation de notre objet contrôleur
+    private AuthentificationController authController;
+
+    public AuthentificationView(AuthentificationController authentificationController)
+    {
+        this.authController = authentificationController;
+        this.setTitle("Gestion Stage");
+        this.setSize(500, 500);
+        this.setContentPane(container);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setLocationRelativeTo(null);
+        bConnexion.addActionListener(new BConnexionListener());
+        bInscription.addActionListener(new BInscriptionListener());
+        this.pack();
+        this.setVisible(true);
+    }
+
+    class BConnexionListener implements ActionListener
+    {
+        public void actionPerformed(ActionEvent e)
+        {
+            authController.setUserName(tfUserName.getText());
+            authController.setMdp(tfMdp.getText());
+
+            authController.authentification();
+        }
+    }
+
+    class BInscriptionListener implements ActionListener
+    {
+        public void actionPerformed(ActionEvent e)
+        {
+            StatutInscriptionView statutInscriptionView = new StatutInscriptionView();
+            AuthentificationView.this.dispose();
+        }
+    }
 }
