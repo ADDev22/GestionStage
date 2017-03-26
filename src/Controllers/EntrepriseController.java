3de3d3a116802db526d01ce7c1982e88e506167b
A2lam <mohamedallam31@hotmail.com>
2017-03-26 19:40:39 +0200

    Ajout de la mmethode se chargeant de l'inscription

--- src/Controllers/EntrepriseController.java
+++ src/Controllers/EntrepriseController.java
@@ -1,11 +1,28 @@
 package Controllers;
 
+import Models.DAO.DAO;
+import Models.DAO.EntrepriseDAO;
+import Models.Entreprise;
+
+import javax.swing.*;
+
 public class EntrepriseController {
 	  
-	
+	//Déclaration
+	DAO<Entreprise> DAO = new EntrepriseDAO();
+
 	public void deposeOffre()
 	{
 		// IHM ->pour deposer un offre
 	}
 
+	//Ajout d'une entreprise
+	public void insert(Entreprise e)
+	{
+		DAO.create(e);
+
+		//Affichage de la boite de dialogue
+		JOptionPane jOP = new JOptionPane();
+		jOP.showMessageDialog(null, "Enregistrement réussie", "Information", JOptionPane.INFORMATION_MESSAGE);
+	}
 }
