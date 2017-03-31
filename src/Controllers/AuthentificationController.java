8ab46534d0ba7c1e3c7e420046e2cc0976da3466
A2lam <mohamedallam31@hotmail.com>
2017-03-31 11:48:24 +0200

    Travail sur les redirections selon l'utilisateur connecte

--- src/Controllers/AuthentificationController.java
+++ src/Controllers/AuthentificationController.java
@@ -1,14 +1,22 @@
 package Controllers;
 
-import Models.DAO.IAuthentification;
-import Models.DAO.UtilisateurDAO;
+import Models.Administrateur;
+import Models.DAO.*;
+import Models.Entreprise;
+import Models.Etudiant;
 import Models.Utilisateur;
+import Views.Administrateur.AdministrateurAccueilView;
+import Views.Administrateur.ResultSetTableModel;
+
+import javax.swing.*;
 
 public class AuthentificationController
 {
 	//Déclaration
 
 	IAuthentification iAuth;
+	Utilisateur utilisateur = new Utilisateur();
+	UtilisateurDAO uDAO = new UtilisateurDAO();
 
 	//Constructeur
 
@@ -22,9 +30,36 @@ public class AuthentificationController
 	{
 		if(iAuth.connexion(u.getUtilisateurUserName(), u.getUtilisateurMdp()))
 		{
+			utilisateur = this.iAuth.find(u.getUtilisateurUserName());
+			if (utilisateur.getFonction().getFonctionId() == 1)
+			{
+				Administrateur admin = new Administrateur();
+				DAO<Administrateur> aDAO = new AdministrateurDAO();
+				admin = aDAO.findTypeUser(utilisateur.getUitilisateurId());
+
+				//Affichage de la boite de dialogue
+				JOptionPane jOP = new JOptionPane();
+				jOP.showMessageDialog(null, "Connexion réussie : " + admin.getAdministrateurNom() + " " + admin.getAdministrateurPrenom(), "Information", JOptionPane.INFORMATION_MESSAGE);
+
+				AdministrateurDAO administrateurDAO = new AdministrateurDAO();
+				AdministrateurController administrateurController = new AdministrateurController(administrateurDAO);
+				ResultSetTableModel offresRS = new ResultSetTableModel(new OffreStageDAO().listeOffres());
+				AdministrateurAccueilView administrateurAccueilView = new AdministrateurAccueilView(administrateurController, offresRS);
+			}
+			else if (utilisateur.getFonction().getFonctionId() == 2)
+			{
+				Entreprise ent = new Entreprise();
+				DAO<Entreprise> eDAO = new EntrepriseDAO();
+				ent = eDAO.findTypeUser(utilisateur.getUitilisateurId());
+			}
+			else
+			{
+				Etudiant et = new Etudiant();
+				DAO<Etudiant> eDAO = new EtudiantDAO();
+				et = eDAO.findTypeUser(utilisateur.getUitilisateurId());
+			}
 			return true;
 		}
-
 		return false;
 	}
 }
