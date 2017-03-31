2e780afbe2201f43948229f47b39b7fa812d1e56
A2lam <mohamedallam31@hotmail.com>
2017-03-31 11:56:50 +0200

    Creation de la classe de traitement de table

--- src/Views/Administrateur/ResultSetTableModel.java
+++ src/Views/Administrateur/ResultSetTableModel.java
@@ -1,7 +1,92 @@
 package Views.Administrateur;
 
+import javax.swing.table.AbstractTableModel;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+
 /**
  * Created by Allam on 27/03/2017.
  */
-public class ResultSetTableModel {
+public class ResultSetTableModel extends AbstractTableModel
+{
+    //Déclaration
+    private ResultSet resultSet;
+    private ResultSetMetaData resultSetMetaData;
+
+    public ResultSetTableModel(ResultSet resultSet)
+    {
+        this.resultSet = resultSet;
+
+        try
+        {
+            this.resultSetMetaData = resultSet.getMetaData();
+        }
+        catch (SQLException e)
+        {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public int getRowCount()
+    {
+        try
+        {
+            resultSet.last();
+            return resultSet.getRow();
+        }
+        catch (SQLException e)
+        {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            return 0;
+        }
+    }
+
+    @Override
+    public int getColumnCount()
+    {
+        try
+        {
+            return resultSetMetaData.getColumnCount();
+        }
+        catch (SQLException e)
+        {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            return 0;
+        }
+    }
+
+    @Override
+    public Object getValueAt(int rowIndex, int columnIndex) {
+        try
+        {
+            resultSet.absolute( rowIndex + 1);
+            return resultSet.getObject(columnIndex + 1);
+        }
+        catch (SQLException e)
+        {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    @Override
+    public String getColumnName(int column)
+    {
+        try
+        {
+            return resultSetMetaData.getColumnName(column + 1);
+        }
+        catch (SQLException e)
+        {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            return "";
+        }
+    }
 }
