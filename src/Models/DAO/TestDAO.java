1cfdda68edb3ebdcdd5d6d476303aa61abc81705
DRAME <arouna.drame@hotmail.com>
2017-04-01 22:59:42 +0200

    TEST -> OK

--- src/Models/DAO/TestDAO.java
+++ src/Models/DAO/TestDAO.java
@@ -1,44 +1,51 @@
 package Models.DAO;
 
+import Controllers.EntrepriseController;
+import Controllers.EtudiantController;
 import Models.Entreprise;
 import Models.EtuPostStage;
 import Models.Etudiant;
 import Models.Fonction;
 import Models.OffreStage;
+import Views.Entreprise.EntrepriseInscView;
+import Views.Entreprise.EntrepriseView;
 
 public class TestDAO {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) {		// TODO Auto-generated method stub
+	Fonction f1 =new Fonction(); f1.setFonctionId(2); f1.setFonctionNom("Entreprise");
 		Entreprise e1 = new  Entreprise(); e1.setRaisonSociale("ORANGE");
 		e1.setMail("org@mail.com");e1.setAdresseCodePostaleEnt(75000);e1.setAdresseRueEnt("322");e1.setTel("06545444");e1.setAdresseVilleEnt("PARIS");
 		e1.setSecteurActivite("INFO");
+		e1.setUtilisateurUserName("Sanva");e1.setUtilisateurMdp("root");e1.setFonction(f1);
 		
 	 // Test EntrepriseDAO
-		EntrepriseDAO e1DAO = new EntrepriseDAO();
-		           e1DAO.create(e1);  //Work !!!
-		           System.out.println(e1.getIdEntreprise());
+		  EntrepriseDAO e1DAO = new EntrepriseDAO();
+		        e1DAO.create(e1);  //Work !!!
+		    ///       System.out.println(e1.getIdEntreprise());
 		          //  Entreprise e1f= e1DAO.find(2); Work !!!  System.out.println(e1f.getRaisonSociale());
-		         e1.setIdEntreprise(27);
+		        // e1.setIdEntreprise(27);
 		       // e1.setAdresseCodePostaleEnt(75008);
 		        //e1DAO.update(e1);  Work !!!
 		         //e1DAO.delete(e1); Work !!!
 		        // e1DAO.getAllOffreStage(e1);
 
 	  //Test EtudiantDAO
-	             EtudiantDAO etDAO = new EtudiantDAO(); Fonction f =new Fonction(); f.setFonctionNom("Etudiant");;
-	                Etudiant et = new Etudiant(); f.setFonctionId(3);
-	                  et.setNom("ALAM");et.setPrenom("MHD"); et.setFonction(f);
-	                   etDAO.create(et);// Work !!!
+	             //EtudiantDAO etDAO = new EtudiantDAO(); Fonction f =new Fonction(); f.setFonctionNom("Etudiant");
+	               // Etudiant et = new Etudiant(); f.setFonctionId(3);
+	                 // et.setNom("ALAM");et.setPrenom("MHD"); et.setFonction(f);
+	                   //etDAO.create(et);// Work !!!
 	                  // System.out.println(etDAO.find(3).getNom()); Work !!!
-	                  et.setDomEtude("INFO");
+	                  //et.setDomEtude("INFO");
 	                   //et.setIdEtudiant(3); //etDAO.update(et); Work !!!
 	                     // etDAO.delete(et); Work !!!
-	// Test OffreStageDAO
+	     //Test OffreStageDAO
 	                  // OffreStageDAO ofDAO = new OffreStageDAO();
-	                   //OffreStage of = new  OffreStage();
-	                   //of.setDateDebut("020817"); of.setDescriptifOffre("PHP");of.setEntreprise(e1);
-	                  //ofDAO.create(of);  //Work !!!
+	                  //OffreStage of = new  OffreStage();
+	                   //of.setDateDebut("020817"); of.setDescriptifOffre("PHP");of.setEntreprise(e1);of.setLibelleOffre("Dev");
+	                   //of.setDescriptifOffre("Dev en language en R");of.setDureeOffre("30 mois");
+	                   //ofDAO.create(of);  //Work !!!
+	                   new EntrepriseInscView();
                   // of.setDescriptifOffre("Blabla");of.setIdOffreStage(1);ofDAO.update(of); Work !!!
 	                  // System.out.println(ofDAO.find(1).getDateDebut()); Work !!!
 	                   //of.setIdOffreStage(1); ofDAO.delete(of); Work !!!
@@ -48,6 +55,10 @@ public class TestDAO {
 	                  //ofEt.setEtudiant(et);ofEt.setOffre(of);
 	                  //ofEtD.create(ofEt);
                    
+	                  //new EntrepriseInscView();
+	               //   new EntrepriseAccView().setVisible(true);
+		  //new EntrepriseView().setVisible(true);;
+	                  
 	                  
 	}	                
 
