a1ac575b910a46ca69571e3643ab7cb21db15383
DRAME <arouna.drame@hotmail.com>
2017-04-03 10:20:41 +0200

    Liste offre -> View Etudiant

---
+++ src/Models/OffreEtuModel.java
@@ -0,0 +1,58 @@
+package Models;
+
+import java.util.ArrayList;
+
+import javax.swing.table.AbstractTableModel;
+
+public class OffreEtuModel extends AbstractTableModel {
+	
+	     private String[] nomC = {"ENTREPISE","LIBELE","DATE DEBUT", "DUREE"};
+	     private ArrayList<String[]>  rows = new  ArrayList<String[]>();
+	     private ArrayList<OffreStage> listOf = new ArrayList<OffreStage>();
+	   
+
+		@Override
+		public int getRowCount(){
+			return rows.size();
+		}
+
+		public ArrayList<OffreStage> getListOf() {
+			return listOf;
+		}
+
+		public void setListOf(ArrayList<OffreStage> listOf) {
+			this.listOf = listOf;
+		}
+
+		@Override
+		public int getColumnCount() {
+			return nomC.length;
+		}
+		@Override
+		public String getColumnName(int column) {
+			// TODO Auto-generated method stub
+			return this.nomC[column];
+		}
+
+		@Override
+		public Object getValueAt(int rowIndex, int columnIndex) {
+			      return rows.get(rowIndex)[columnIndex];
+		}
+		public void loadOffre(ArrayList<OffreStage> ofs)
+		{
+			rows = new  ArrayList<String[]>();
+			for(OffreStage of: ofs)
+			{
+				rows.add(new String[] {of.getEntreprise().getRaisonSociale(),  of.getLibelleOffre(), of.getDateDebut(),of.getDureeOffre()});
+		        listOf.add(of);	     	
+			}
+			fireTableChanged(null);
+		}
+		public void removeRow(int rowIndex) {
+			 rows.remove(rowIndex);
+			 listOf.remove(rowIndex);
+			 fireTableRowsDeleted(rowIndex, rowIndex);
+		 }
+		
+
+	}
