c70059077265041b1b66613b981caa39cc49667d
A2lam <mohamedallam31@hotmail.com>
2017-04-06 11:52:38 +0200

    ajout methode insertion au ControllerAdministrateur

--- src/Controllers/AdministrateurController.java
+++ src/Controllers/AdministrateurController.java
@@ -5,6 +5,8 @@ import Models.DAO.AdministrateurDAO;
 import Models.DAO.DAO;
 import com.mysql.jdbc.interceptors.SessionAssociationInterceptor;
 
+import javax.swing.*;
+
 /**
  * Created by Allam on 26/03/2017.
  */
@@ -19,6 +21,14 @@ public class AdministrateurController
     public AdministrateurController(AdministrateurDAO aDAO)
     {
         this.DAO = aDAO;
+    }
 
+    public void insert(Administrateur a)
+    {
+        this.DAO.create(a);
+
+        //Affichage de la boite de dialogue
+        JOptionPane jOP = new JOptionPane();
+        jOP.showMessageDialog(null, "Enregistrement réussie", "Information", JOptionPane.INFORMATION_MESSAGE);
     }
 }
